eefc7492f9526d3819e1b427e06ba24768165f63
mbarreras23 <mbarreras.s95@gmail.com>
2025-01-27 20:53:27 -0700

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = collect([
+            [
+                "email" => "admin@poslink.com.mx",
+                "name" => "admin",
+                "surname" => "root",
+                "second_surname" => "root",
+                "password" => Hash::make("laravel_123"),
+                "role" => "admin"
+            ]
+        ]);
+
+        $users->each(function ($user) {
+            $new_user = User::updateOrCreate(
+                [
+                    "email" => $user["email"]
+                ],
+                [
+                    "name" => $user["name"],
+                    "surname" => $user["surname"],
+                    "second_surname" => $user["second_surname"],
+                    "password" => $user["password"]
+                ]
+            );
+
+            $new_user->assign($user["role"]);
+        });
+    }
+}
